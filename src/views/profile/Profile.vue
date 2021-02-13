5bcea83f5dbe82780ca748907a69ea0b3409ee8c
bigMARAC <marcao.ia18@gmail.com>
2021-02-13 02:03:51 -0300

    User update implementation

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -8,57 +8,109 @@
       </v-col>
     </v-row>
     <v-col class="pt-4">
-      <v-text-field outlined v-model="$store.state.user.nome" label="Nome"></v-text-field>
-      <v-textarea outlined v-model="$store.state.user.descricao" label="Biografia"></v-textarea>
+      <v-text-field
+        outlined
+        v-model="$store.state.user.nome"
+        label="Nome"
+      ></v-text-field>
+      <v-textarea
+        outlined
+        v-model="$store.state.user.descricao"
+        label="Biografia"
+      ></v-textarea>
     </v-col>
     <v-row class="px-6">
-      <v-chip-group column center-active>
-        <v-chip v-for="subject in $store.state.user.subjects" :key="subject.nome" color="primary" label> {{ subject.nome }}</v-chip>
+      <v-chip-group
+        column
+        center-active
+        active-class="primary"
+        multiple
+        v-model="materias"
+      >
+        <v-chip
+          v-for="subject in $store.state.user.subjects"
+          :key="subject.nome"
+          :value="subject.id"
+          label
+          @click="select(subject)"
+        >
+          {{ subject.nome }}</v-chip
+        >
       </v-chip-group>
     </v-row>
-    <v-btn
-      bottom
-      right
-      fixed
-      fab
-      color="primary"
-      @click="save"
-    >
+    <v-btn bottom right fixed fab color="primary" @click="save">
       <v-icon>mdi-content-save</v-icon>
     </v-btn>
   </v-form>
 </template>
 
 <script>
-import Vue from "vue"
-import UpdateUserRequest from "./../../requests/UpdateUserRequest.js"
+import Vue from "vue";
+import UpdateUserRequest from "./../../requests/UpdateUserRequest.js";
+import MeRequest from "./../../requests/MeRequest.js";
 
 export default Vue.extend({
   data() {
     return {
-      user: {}
-    }
+      materias: [],
+      user: {},
+      ids: [],
+    };
   },
   components: {},
   methods: {
     setProfilePicture() {
-      return this.$store.state.user.foto ?? "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png"
+      return (
+        this.$store.state.user.foto ??
+        "https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png"
+      );
+    },
+    select(subject) {
+      let ocurrences = this.ids.find((el) => el.id == subject.id);
+      if (ocurrences !== undefined) {
+        this.ids = this.ids.filter((el) => el.id != subject.id);
+      } else {
+        this.ids.push(subject);
+      }
+      const ids = this.ids.map((el) => el.id);
+      console.log(ids);
     },
     async save() {
+      const ids = this.ids.map((el) => el.id);
+
       const request = new UpdateUserRequest(this.$store.state.user.token, {
         nome: this.$store.state.user.nome,
-        descricao: this.$store.state.user.descricao,
-        foto: this.$store.state.user.foto,
-      })
+        aluno_id: this.$store.state.user.id,
+        ids,
+      });
 
-      const response = await request.send()
+      await request.send();
 
-      return response
-    }
+      const meRequest = new MeRequest(this.$store.state.user.token);
+      const meResponse = await meRequest.send();
+
+      this.$store.dispatch("triggerSetUser", meResponse.data.aluno);
+
+      await this.$store.dispatch("loadSubjects", this.$store.state.user.token);
+      await this.$store.dispatch("loadUsers", this.$store.state.user.token);
+
+      this.$store.commit("saveUser");
+
+      this.materias = []
+      this.ids = []
+      this.listUserSubjects();
+
+      return meResponse;
+    },
+    listUserSubjects() {
+      for (const materia of this.$store.state.user.materias) {
+        this.materias.push(materia.id);
+      }
+    },
   },
   mounted() {
-    this.user = this.$store.state.user
-  }
+    this.listUserSubjects();
+  },
 });
 </script>
 
