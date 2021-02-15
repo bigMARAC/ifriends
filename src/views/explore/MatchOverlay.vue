8d844528a8fc140c66338d7ce8a6fe158f7ae915
bigMARAC <marcao.ia18@gmail.com>
2021-02-15 01:03:09 -0300

    Trying to fix MatchOverlay

--- src/views/explore/MatchOverlay.vue
+++ src/views/explore/MatchOverlay.vue
@@ -1,14 +1,24 @@
 <template>
   <v-row align="center" justify="space-around" v-if="user !== undefined">
     <v-overlay :value="overlay" :opacity="0.9">
-      <v-card-title>Você combina com essa pessoa!</v-card-title>
-      <v-divider class="mx-4"></v-divider>
-      <v-card-text class="text-center">
-        <div>Entre em contato com {{ user.nome }}:</div>
-      </v-card-text>
-      <v-card-actions class="mt-3">
-        <v-row align="center" justify="space-around">
-          <v-col cols="3">
+      <v-row>
+        <v-col cols="12">
+          <v-card-title>Você combina com essa pessoa!</v-card-title>
+          <v-divider class="mx-4"></v-divider>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <v-card-text class="text-center">
+            <div>Entre em contato com {{ user.nome }}:</div>
+          </v-card-text>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-card-actions class="mt-3">
+          <v-col cols="6">
             <v-btn
               large
               :href="`https://www.instagram.com/${user.instagram}`"
@@ -18,7 +28,7 @@
               <v-icon color="#ff68be">mdi-instagram</v-icon></v-btn
             >
           </v-col>
-          <v-col cols="3">
+          <v-col cols="6">
             <v-btn
               large
               elevation="5"
@@ -28,32 +38,38 @@
               <v-icon color="success">mdi-whatsapp</v-icon></v-btn
             >
           </v-col>
-          <v-col cols="12">
-            <v-divider class="mx-auto my-5"></v-divider>
-          </v-col>
-          <v-col cols="3">
-            <v-btn @click="nextUser" color="success" outlined> C </v-btn>
-          </v-col>
-        </v-row>
-      </v-card-actions>
+        </v-card-actions>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <v-divider class="mx-auto my-5"></v-divider>
+        </v-col>
+      </v-row>
+
+      <v-row align="center" justify="space-around">
+        <v-col cols="3">
+          <v-btn @click="nextUser" color="success" outlined> Continuar </v-btn>
+        </v-col>
+      </v-row>
     </v-overlay>
   </v-row>
 </template>
 
 <script>
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
   props: {
     overlay: {
       type: Boolean,
-      default: false
+      default: false,
     },
-    user: {}
+    user: {},
   },
   methods: {
     nextUser() {
-      this.$emit('ifriends_next', true)
-    }
-  }
+      this.$emit("ifriends_next", true);
+    },
+  },
 });
 </script>
